8548453fdede4bbbb500961a9011f45e3940588e
Sagar Batham <shivsagar6912@gmail.com>
2026-02-05 13:49:12 +0530

    Updated

---
+++ JavaScript/Chapter_2/script.js
@@ -0,0 +1,36 @@
+//Data Types
+
+//1.Primitive Data Types
+ //-Number
+ //-String('A','Hello)
+ //-Boolean(true,false)
+ //-Undefined
+ //-null
+ //-Symbol
+ //-BigInt
+
+//2.Reference Data Types
+ //-Array 
+ //-Object
+ //-Function
+
+//  var a=10;
+//  console.log(a);
+
+// var a =prompt('Enter Your Age '); //Prompt Enter all thing as a String
+// console.log(a)
+// var a= prompt('Enter a First Number: ');
+// var realA=Number(a);
+// var b=prompt('Enter a Second Number: ');
+// var realB=Number(b);
+// console.log(a+b);
+// console.log(realA+realB);
+
+//CONDTIONALS
+var a=Number(prompt('Enter a Age'));
+if(a>=18){
+    console.log('18 Plus');
+}else{
+    console.log('Minor');
+}
+
