02c2f631bad689cae4a951e722592bdad49281de
Sagar Batham <shivsagar6912@gmail.com>
2026-02-05 11:46:13 +0530

    JavaScript Started

---
+++ JavaScript/Chapter_1/script.js
@@ -0,0 +1,18 @@
+// console.log("Hello World")
+
+// console.warn("This is Warnig")
+
+// console.error("This is Error")
+var a=0
+
+// alert("Why You")
+
+// confirm("Are You 18+")
+
+// prompt("Enter Details")
+var a=confirm("Are You 18+");
+console.log(a);
+
+const Dulha='Man';
+const Dulhan='Women';
+console.log(Dulha,'weds',Dulhan);
