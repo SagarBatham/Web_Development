ededc96369cedfd7837cfb7e3b9c5575717a133b
Sagar Batham <shivsagar6912@gmail.com>
2026-02-14 16:36:51 +0530

    Add console.group examples and search methods to Chapter 7 script

--- JavaScript/Chapter_7/script.js
+++ JavaScript/Chapter_7/script.js
@@ -5,3 +5,14 @@ console.info("Hello");
 console.table({Name:"Sagar",Class:12});
 
 console.log(typeof "Str");
+
+console.group("Animal Name");
+    console.log("Hathi");
+    console.log("Sher");
+    console.log("Cheetah");
+console.groupEnd();
+
+//Search and IndexOf
+let c="JavaScript";
+console.log(c.indexOf("Script"));
+console.log(c.search("Script"));
