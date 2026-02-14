34df68b870cb3d3b13cd0de85d64a4d47ad94781
Sagar Batham <shivsagar6912@gmail.com>
2026-02-14 16:10:12 +0530

    Add JavaScript files for Chapter 7 and update Chapter 6 script with array and object examples

--- JavaScript/Chapter_6/script.js
+++ JavaScript/Chapter_6/script.js
@@ -1,5 +1,20 @@
 //Array and Objects
 
+//Array
 var a=[10,2,3,4];
 console.log(a[0]);
-var arr=new Array();
+var arr=new Array(5);
+for (let i = 0; i < arr.length; i++) {
+    arr[i]=i;
+}
+
+//Objects
+var obj={
+    name:"Sagar",
+    age:21,
+    email:"shivsagar6912@gmail.com",
+    uni:"Aktu",
+    isStudy:true
+};
+
+console.log(obj.email);
---
+++ JavaScript/Chapter_7/script.js
@@ -0,0 +1,7 @@
+console.log("Hello");
+console.warn("Hello");
+console.error("Hello");
+console.info("Hello");
+console.table({Name:"Sagar",Class:12});
+
+console.log(typeof "Str");
