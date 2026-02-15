48c0f49ae496278675c61dff9249e36979e156f5
Sagar Batham <shivsagar6912@gmail.com>
2026-02-15 21:11:14 +0530

    Add button and click event functionality to Chapter 2; create new Card components with styles and scripts

---
+++ DOM/Card/script.js
@@ -0,0 +1,22 @@
+ var clicked=true;
+document.addEventListener("click", function () {
+    if (clicked) {
+        var a = document.querySelector(".box h3");
+        a.innerHTML = "Now We Are Friends"
+        var b = document.querySelector(".box button");
+        b.innerHTML = "Remove Friend";
+        b.style.backgroundColor = "green";
+        clicked=false;
+        console.log("Click 1")
+    }else{
+        var a = document.querySelector(".box h3");
+        a.innerHTML = "You want to be Friend"
+        var b = document.querySelector(".box button");
+        b.innerHTML = "Accept Request";
+        b.style.backgroundColor = "red";
+        clicked=true;
+        console.log("Click 2")
+    }
+    })
+
+
--- DOM/Chapter_2/script.js
+++ DOM/Chapter_2/script.js
@@ -1,3 +1,12 @@
-// var boxy=document.querySelector("#box")
-var boxy=document.getElementById("box")
-boxy.style.backgroundColor="green"
+// var boxy=document.querySelector("#box");
+var boxy=document.getElementById("box");
+boxy.style.backgroundColor="green";
+boxy.innerHTML="<h1>Html Code</h1>";
+boxy.textContent="<h1>As it is Text</h1>";
+
+var b=document.querySelector("button");
+document.addEventListener("click",function(){
+    b.style.backgroundColor="red";
+    b.textContent="Already Clicked";
+    boxy.style.backgroundColor='Yellow';
+})
