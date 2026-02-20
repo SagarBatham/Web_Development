06df7254c57c4f12a252ed33d50246da6728b2f8
Sagar Batham <shivsagar6912@gmail.com>
2026-02-20 22:45:13 +0530

    Create Elements

--- DOM/Chapter_4/script.js
+++ DOM/Chapter_4/script.js
@@ -3,3 +3,8 @@ var b=a.getAttribute("id");
 a.setAttribute("id","heroine")
 console.log(a);
 console.log(b);
+
+var b=document.createElement("h1");
+b.innerHTML="Hello Kaise Ho";
+var body=document.querySelector("body");
+body.appendChild(b);
