a31e9608aa63113452e78e2aab9ae0fa31b831e9
Sagar Batham <shivsagar6912@gmail.com>
2026-02-22 21:22:11 +0530

    Updates

--- Event_Bubbling/Chapter_1/script.js
+++ Event_Bubbling/Chapter_1/script.js
@@ -1,36 +1,64 @@
-var arr=[{
-    name:"Sagar",
-    age:20,
-    img:"https://images.unsplash.com/photo-1515041219749-89347f83291a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FydG9vbnxlbnwwfHwwfHx8MA%3D%3D"
-    ,place:"Etawah"
-    ,marr:false
-} ,{
-    name:"Shiva",
-    age:21,
-    img:"https://images.unsplash.com/photo-1531214159280-079b95d26139?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGNhcnRvb258ZW58MHx8MHx8fDA%3D"
-    ,place:"Noida"
-    ,marr:false
-},{
-    name:"ShivSagar",
-    age:23,
-    img:"https://images.unsplash.com/photo-1606663889134-b1dedb5ed8b7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2FydG9vbnxlbnwwfHwwfHx8MA%3D%3D"
-    ,place:"Delhi"
-    ,marr:false
+var arr = [{
+    name: "Sagar",
+    age: 20,
+    img: "https://images.unsplash.com/photo-1515041219749-89347f83291a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FydG9vbnxlbnwwfHwwfHx8MA%3D%3D"
+    , place: "Etawah"
+    , Marriage: false,
+    isStatus: "Stranger",
+
+}, {
+    name: "Shiva",
+    age: 21,
+    img: "https://images.unsplash.com/photo-1531214159280-079b95d26139?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGNhcnRvb258ZW58MHx8MHx8fDA%3D"
+    , place: "Noida"
+    , Marriage: false,
+    isStatus: "Stranger",
+}, {
+    name: "ShivSagar",
+    age: 23,
+    img: "https://images.unsplash.com/photo-1606663889134-b1dedb5ed8b7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2FydG9vbnxlbnwwfHwwfHx8MA%3D%3D"
+    , place: "Delhi"
+    , Marriage: false,
+    isStatus: "Stranger",
 }]
 
-var sum="";
-var main=document.querySelector("#main");
-arr.forEach(function(elem,idxe,dets){
-    sum=sum+ `<div class="card">
+var btn = document.querySelector(".card button");
+var main = document.querySelector("#main");
+
+
+function herofun() {
+    var sum = "";
+
+    arr.forEach(function (elem, idxe) {
+        var buttonText = elem.isStatus == "Stranger"
+            ? "Add Friend"
+            : "Remove Friend";
+
+        sum = sum + `<div class="card">
         <img src=${elem.img} alt="">
         <h1>${elem.name}</h1>
         <h5>${elem.age}</h5>
         <h4>${elem.place}</h4>
-        <h3>Married:${elem.marr}</h3>
-        <h4>Stranger</h4>
-        <button>Add Friend</button>
+        <h3>Married:${elem.Marriage}</h3>
+        <h2>${elem.isStatus}</h2>
+        <button id=${idxe}>${buttonText}</button>
     </div>`
-    console.log(dets);
+    })
+
+    main.innerHTML = sum;
+}
+
+herofun();
+
+main.addEventListener("click", function (dets) {
+    var a = arr[dets.target.id];
+    if (a.isStatus == "Stranger") {
+        a.isStatus = "Friends";
+        
+    } else {
+        a.isStatus = "Stranger";
+        
+    }
+    herofun();
 })
 
-main.innerHTML=sum;
