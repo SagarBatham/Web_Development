1529023de788878f41b63ee693691b9f7cd0ddff
Sagar Batham <shivsagar6912@gmail.com>
2026-04-29 22:08:01 +0530

    Started a Moody Player Project

--- Backend_Stack/DbConnection/server.js
+++ Backend_Stack/DbConnection/server.js
@@ -1,7 +1,47 @@
 var express=require("express")
 var connectToDb=require("./src/db/db")
+var notemodel=require("./src/models/notes.model")
 connectToDb()
 const app=express();
+app.use(express.json())
+
+app.post("/notes",(req,res)=>{
+    const {title,desc}=req.body
+
+    notemodel.create({
+        title,desc
+    })
+
+    res.json({
+        message:"Note Created Succesfully"
+    })
+})
+
+app.delete("/notes/:id",async(req,res)=>{
+    const id=req.params.id
+
+    await notemodel.findOneAndDelete({_id:id})
+
+    res.json({
+        message:"Notes Deleted Successfully"
+    })
+})
+
+app.get("/notes",async(req,res)=>{
+    const users=await notemodel.find()
+    res.json({
+        user:users,
+        message:"All Message are Here"
+    })
+})
+
+app.patch("/notes/:id",async(req,res)=>{
+    const id=req.params.id
+    await notemodel.findOneAndUpdate({_id:id},req.body)
+    res.json({
+        message:"Notes Updated Succesfully"
+    })
+})
 
 app.listen(3000,()=>{
     console.log("Server running on Port 3000");
--- Backend_Stack/DbConnection/src/db/db.js
+++ Backend_Stack/DbConnection/src/db/db.js
@@ -1,7 +1,7 @@
 var mongoose=require("mongoose")
 
 function connectToDb() {
-    mongoose.connect("mongodb://sagar:nL2g47aknVh1x0yi@ac-w4l0kzu-shard-00-00.zmtvmza.mongodb.net:27017,ac-w4l0kzu-shard-00-01.zmtvmza.mongodb.net:27017,ac-w4l0kzu-shard-00-02.zmtvmza.mongodb.net:27017/?ssl=true&replicaSet=atlas-gwifr0-shard-0&authSource=admin&appName=Backend")
+    mongoose.connect("mongodb://sagar:nL2g47aknVh1x0yi@ac-w4l0kzu-shard-00-00.zmtvmza.mongodb.net:27017,ac-w4l0kzu-shard-00-01.zmtvmza.mongodb.net:27017,ac-w4l0kzu-shard-00-02.zmtvmza.mongodb.net:27017/learnBackend?ssl=true&replicaSet=atlas-gwifr0-shard-0&authSource=admin&appName=Backend")
     .then(()=>{
     console.log("Db is Connected");  
 })
