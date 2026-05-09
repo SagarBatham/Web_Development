a1ee5fbeaafec7df9833e8c47d55437851d1532d
Sagar Batham <shivsagar6912@gmail.com>
2026-05-09 18:54:22 +0530

    Authenticatin By JWT

--- Backend_Stack/Auth/src/app.js
+++ Backend_Stack/Auth/src/app.js
@@ -1,4 +1,6 @@
 require("dotenv").config()
+const jwt=require("jsonwebtoken")
+const cookieParser=require("cookie-parser")
 const express=require("express")
 const connectToDb=require("../db/db")
 const router=require("./routes/user.routes")
@@ -6,5 +8,6 @@ const router=require("./routes/user.routes")
 connectToDb()
 const app=express()
 app.use(express.json())
+app.use(cookieParser())
 app.use("/auth",router)
 module.exports=app
--- Backend_Stack/Auth/src/routes/user.routes.js
+++ Backend_Stack/Auth/src/routes/user.routes.js
@@ -1,17 +1,33 @@
 const mongoose = require("mongoose")
 const express = require("express")
+const jwt=require("jsonwebtoken")
 const usermodel = require("../model/user.model")
 const router = express.Router()
 
 router.post("/register", async (req, res) => {
-    const newUser = await usermodel.create({
-        username: req.body.username,
-        password: req.body.password
+    const{username,password}=req.body
+    const existUser=await usermodel.findOne({
+        username
     })
-    console.log(newUser);
+    if(existUser){
+        res.status(401).json({
+            msg:"Username is Already Exist"
+        })
+        return
+    }
+    const newUser = await usermodel.create({
+        username,
+        password
+    })
+    console.log(newUser)
+    const token=jwt.sign({id:newUser._id},process.env.JWT_SECRET)
+    res.cookie("token",token,{
+        expires:new Date.now()+1000*60
+    })
     res.status(201).json({
         msg: 'New User Created Successfully',
-        newUser
+        newUser,
+        token
     })
 })
 
@@ -27,7 +43,6 @@ router.post("/login", async (req, res) => {
         return res.status(401).json({
             msg: "Invalid Username"
         })
-
     }
 
     const isPassValid = password == user.password
@@ -42,6 +57,44 @@ router.post("/login", async (req, res) => {
         msg: "User Successfully Login"
     })
 
+})
+
+router.get("/user",async(req,res)=>{
+    console.log(req.cookies);
+    
+    const{token}=req.cookies
+
+    if(!token){
+        return res.status(401).json({
+            msg:"Unauthorized Acess",
+            token
+        })
+    }
+    try {
+        const decoded=jwt.verify(token,process.env.JWT_SECRET)
+        const user=await usermodel.findOne({
+        _id:decoded.id
+    }).select("-password -__v")
+     res.json({
+        msg:"User Fetched",
+        user
+    })
+    console.log(user);
+    } catch (error) {
+         return res.status(401).json({
+            msg:"Invalid Token"
+        })
+    }
+       
+   
+})
+
+router.get("/logout",async(res,rej)=>{
+    res.clearCookie("token")
+
+    res.status(200).json({
+        msg:"User logout Successfully"
+    })
 })
 
 module.exports = router
